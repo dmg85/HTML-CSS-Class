eb46139b97130232e43cc6dde2f17a991ff52f0d
Diego Medina <damgz191@gmail.com>
2023-03-27 20:45:55 -0500

    Last part of the exercise

--- resImages_Traversy/main.js
+++ resImages_Traversy/main.js
@@ -1,6 +1,6 @@
 const slides = document.querySelectorAll('.slide');
-const next = document.querySelectorAll('#next');
-const prev = document.querySelectorAll('#prev');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
 const auto = false;
 const intervalTime = 5000;
 let slideInterval;
@@ -8,15 +8,15 @@ let slideInterval;
 const nextSlide = () => {
     // Get current class
     const current = document.querySelector('.current');
-    // remove current class
+    // Remove current class
     current.classList.remove('current');
     // Check for next slide
-    if(current.nextElementSibling) {
-        // Add current to next next sibling
+    if(current.nextElementSibling){
+        // Add current to next sibling
         current.nextElementSibling.classList.add('current');
-    } else {
+        } else {
         // Add current to start
-        slides[0].classList.add('current')
+        slides[0].classList.add('current');
     }
     setTimeout(() => current.classList.remove('current'));
 }
@@ -24,24 +24,38 @@ const nextSlide = () => {
 const prevSlide = () => {
     // Get current class
     const current = document.querySelector('.current');
-    // remove current class
+    // Remove current class
     current.classList.remove('current');
     // Check for next slide
-    if(current.previousElementSibling) {
-        // Add current to next next sibling
+    if(current.previousElementSibling){
+        // Add current to next sibling
         current.previousElementSibling.classList.add('current');
-    } else {
+        } else {
         // Add current to last
-        slides[slides.length -1].classList.add('current')
+        slides[slides.length -1].classList.add('current');
     }
     setTimeout(() => current.classList.remove('current'));
-};
+}
 
 // Button events
 next.addEventListener('click', e => {
     nextSlide();
+    if(auto)  {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
 });
 
 prev.addEventListener('click', e => {
     prevSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
 });
+
+// Auto slide
+if(auto) {
+    // Run next slide at interval time
+    slideInterval = setInterval(nextSlide, intervalTime);
+}
